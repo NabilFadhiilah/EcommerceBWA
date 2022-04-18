2607acba673b44dd75145d25d009841c489da88f
Nabil Fadhiilah <nabil.10518027@mahasiswa.unikom.ac.id>
2022-04-18 18:23:18 +0700

    CRUD Transaction

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductGalleryController;
+use App\Http\Controllers\TransactionController;
 use App\Models\ProductGallery;
 use Illuminate\Support\Facades\Route;
 
@@ -45,6 +46,7 @@ Route::middleware([
     Route::get('/', [DashboardController::class, 'index'])->name('index');
     Route::middleware(['admin',])->group(function () {
         Route::resource('product', ProductController::class);
-        Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only('index', 'create', 'store', 'destroy');
+        Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only(['index', 'create', 'store', 'destroy']);
+        Route::resource('transaction', TransactionController::class)->only(['index', 'show', 'edit', 'update']);
     });
 });
